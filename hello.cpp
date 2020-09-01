78d3135a9393b5544739f0228997bb7445d648a6
Sahil goyal <s.g.navingoyal@gmail.com>
2020-09-02 04:29:15 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -6,8 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout<<"sahil";
-  cout<<"j";
   cout << "I have successfully built and run\n";
-  //hhhhh
   return 0;
 }
